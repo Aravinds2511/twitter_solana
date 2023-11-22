77b7c69546dcb111c490fe2b28cfe3ca09413ea9
arvndblocker"nn <aravindvasang@gmail.com>
2023-11-22 16:47:47 +0530

    tweet

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -4,6 +4,8 @@ import { Connection, PublicKey } from "@solana/web3.js";
 import { AnchorProvider, Program } from "@project-serum/anchor";
 import idl from "../../../target/idl/solana_twitter.json";
 
+const preflightCommitment = "processed";
+const commitment = "processed";
 const programID = new PublicKey(idl.metadata.address);
 let workspace = null;
 
@@ -11,9 +13,13 @@ export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection("http://127.0.0.1:8899");
+  const connection = new Connection("http://127.0.0.1:8899", commitment);
   const provider = computed(
-    () => new AnchorProvider(connection, wallet.publicKey)
+    () =>
+      new AnchorProvider(connection, wallet.value, {
+        preflightCommitment,
+        commitment,
+      })
   );
   const program = computed(() => new Program(idl, programID, provider.value));
 
