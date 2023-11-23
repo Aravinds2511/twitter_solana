c28277104214d19f5a1cd1e79adc71ea9a7cd46e
arvndblocker"nn <aravindvasang@gmail.com>
2023-11-23 17:51:45 +0530

    changes

--- app/vue.config.js
+++ app/vue.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig({
         util: false,
         path: false,
         stream: false,
-        http: require.resolve("stream-http"),
+        // http: require.resolve("stream-http"),
         https: require.resolve("https-browserify"),
         zlib: require.resolve("browserify-zlib"),
         url: require.resolve("url/"),
